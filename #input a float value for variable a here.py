4f3475bc7f0e3687227c6f6fa900fe9a2e556ae5
Ronilson Leal Gomes <userpv25770@estgl.ipv.pt>
2022-11-02 10:28:51 +0000

    Ronilson Leal Gomes

---
+++ #input a float value for variable a here.py
@@ -0,0 +1,26 @@
+
+a=float(input("Diz me o número: "))
+print("O numero que introduziu:" ,a)
+
+#Makin de guerra 
+b=float(input("Diz me outro número: "))
+print("O numero introduzido:" , b)
+
+#Nuca esqueças
+print("\n9A soma dos dois numeros:",a+b)
+print("A multiplicação dos dois numeros:" ,a*b)
+print("A divisão dos dois numeros:" ,a/b)
+print("A subtração dos dois numeros:" ,a-b)
+name = input("Enter your name: ")
+print("Hello, " + name + ". Nice to meet you!")
+#hgfjrgjrfngfdn
+print("\nPress Enter to end the program")
+input()
+print("The End.")
+#hntffg
+x = int(input("Enter a number: ")) # The user enters 2
+print(x * "5")
+#fhcchgccc
+x = input("Enter a number: ") # The user enters 2
+print(type(x))
+ 
